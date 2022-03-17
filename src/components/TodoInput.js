ff3a8274dc472bb67536cfcf8dc4f6f8aec2b61a
SUNGIL <s0wnd@SUNGILui-MacBookPro.local>
2022-03-17 16:07:11 +0900

    10. 컴포넌트 상태 중앙 집중화

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,24 +1,11 @@
-import React, {useState} from "react";
+import React from "react";
 import styles from "../Todo.module.css";
 
-const TodoInput=( { onInsert} ) =>{
-    // return <h1>TodoInput</h1>;
-    const [value, setValue] = useState("");
-    // 변경 이벤트 처리
-    const onChange = (e) => {
-        setValue(e.target.value);
-    };
-    //submit 이벤트 처리
-    const onSubmit = (e) => {
-        e.preventDefault();
-        onInsert(value);
-        setValue("");
-    };
-
+const TodoInput=( { input, onChange, onSubmit} ) =>{
     return (
         <div className={styles.input}>
             <form onSubmit={onSubmit}>
-                <input placeholder="할 일을 입력하세요" value={value} onChange={onChange}/>
+                <input placeholder="할 일을 입력하세요" value={input} onChange={onChange}/>
                 <button type="submit">추가</button>
             </form>
         </div>
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,4 +1,4 @@
-import React, { useState , useRef } from "react";
+import React, { useState , useRef, useCallback } from "react";
 import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
 import TodoFooter from "./TodoFooter";
@@ -7,42 +7,59 @@ import TodoHeader from "./TodoHeader";
 // 빈 배열 초기값 설정
 const Todos=() => {
     const [todos, setTodos] = useState([]);
+    const [input, setInput] = useState("");
+
     // 로컬 변수 정의
     const nextId = useRef(1);
 
-    const onInsert=(text) => {
+    const onInsert=useCallback((text) => {
         const todo = {
             id: nextId.current,
             text,
             done: false
         };
 
-        setTodos(todos.concat(todo));
+        // setTodos(todos.concat(todo));
+        setTodos((todos) => todos.concat(todo));
         nextId.current += 1;
-    };
+    }, []);
 
     // Todo 항목 삭제 이벤트처리
-    const onRemove = (id) => {
-        setTodos(todos.filter((todo) => todo.id !== id));
-    };
+    const onRemove = useCallback((id) => {
+        // setTodos(todos.filter((todo) => todo.id !== id));
+        setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    }, []);
 
     //완료처리 이벤트 처리
-    const onToggle = (id) => {
-        setTodos(todos.map(
-            (todo) =>
-                todo.id === id ? {...todo, done: !todo.done } : todo
-        ));
-    };
+    const onToggle= useCallback((id) => {
+        setTodos((todos) =>
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, done: !todo.done } : todo
+            ) 
+        );
+    }, []);
 
     // 모두 삭제
-    const onClearAll = () => {
-        setTodos([]);
-    };
+    const onClearAll = useCallback(() => {
+        // setTodos([]);
+        setTodos(() => []);
+    }, []);
+
+    const onChange = useCallback((e) => {
+        setInput(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback((e) => {
+        e.preventDefault();
+
+        onInsert(input);
+        setInput("");
+    }, [onInsert, input]);
 
         return (
             <div>
                 <TodoHeader/>
-                <TodoInput onInsert={onInsert}/>
+                <TodoInput  input={input} onChange={onChange} onSubmit={onSubmit}/>
                 <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
                 <TodoFooter onClearAll={onClearAll}/>
             </div>
